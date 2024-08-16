77f80c2e5f3cc782739aa82fe9bb12cf9d263278
Cyber-jatim607 <faruqsibucilfaruqsibocil@gmail.com>
2024-08-16 13:18:47 +0700

    Add files via upload

---
+++ Wa.sh
@@ -0,0 +1,117 @@
+#!/system/bin/sh
+
+# color
+blue='\033[34;1m'
+green='\033[32;1m'
+purple='\033[35;1m'
+cyan='\033[36;1m'
+red='\033[31;1m'
+white='\033[37;1m'
+yellow='\033[33;1m'
+
+# pembersih text
+clear
+
+sleep 1
+# tampilan
+toilet -f mono12 -F metal Hk-Wa
+sleep 1
+ echo
+echo -e $blue" ========================================="
+echo -e $green" Author : MR TEKNO VRS"
+echo " Team   : Preman Cyber I"
+ecgo -e $blue" ========================================="
+ echo
+sleep 1
+ echo
+echo -e $red" ###########################################"
+echo -e $yellow"         special Whatsapp"
+echo " 1).sadap Whatsapp"
+echo " 2).spam whatsapp"
+echo " 3).nambah kontak"
+echo " 4).keluar"
+echo -e $red" ###########################################"
+ echo -e $white
+sleep 1
+read -p " pilih : " spekto
+
+if [ $spekto = 1 ]
+ then
+sleep 1
+echo " masukkan nomor target"
+read -p " nomor : " targ
+sleep 1
+echo " loading...."
+echo -e $cyan" 10%"
+sleep 2
+echo -e $red" 20%"
+sleep 2
+echo -e $cyan" 30%"
+sleep 2
+echo -e $red" 40%"
+sleep 2
+echo -e $cyan" 50%"
+sleep 2
+echo -e $red" 60%"
+sleep 2
+echo -e $cyan" 70%"
+sleep 2
+echo -e $red" 80%"
+sleep 2
+echo -e $cyan" 90%"
+sleep 2
+echo -e $red" 100%"
+sleep 2
+ echo
+echo -e $white" tunggu 15-20 menit"
+sleep 1
+echo " selesai √√√"
+sleep 1
+ echo
+fi
+
+if [ $spekto = 2 ]
+ then
+sleep 1
+pkg install php
+git clone https://github.com/siputra12/prank
+cd prank
+php wa.php
+fi
+
+if [ $spekto = 3 ]
+ then
+sleep 1
+echo -e $red "Loading"
+sleep 1
+echo -e $blue "+6281455119351"
+sleep 1
+echo -e $green "+6285745151698"
+sleep 1
+echo -e $cyan "+6281250274632"
+sleep 1
+echo -e $purple "+6285704868189"
+sleep 1
+echo -e $blue "+6282133059248"
+sleep 1
+echo -e $red "+6285892735649"
+sleep 1
+echo -e $green "+6281358841162"
+sleep 1
+echo -e $cyan "+6281216305976"
+sleep 1
+echo -e $purple "+6281333398025"
+sleep 1
+echo -e $blue "Terakhir Kontak MrZhu"
+sleep 1
+echo -e $blue "-0859175727510-"
+fi
+
+if [ $spekto = 4 ]
+ then
+sleep 1
+echo -e $white" jangan lupa nama gw ya"
+sleep 1
+echo " dada"
+sleep 2
+exit
